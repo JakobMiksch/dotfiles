3518f538a453315946ba72da1fdc0456997a45f2
Jakob Miksch <jakob@meggsimum.de>
2021-07-08 21:09:43 +0200

    Formatting

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -6,16 +6,30 @@ make_link(){
     ln -s -i -v "$(readlink -f ${1})" ${TARGET_DOTFILE_PATH}
 }
 
+# Tmux
 make_link tmux.conf     .tmux.conf
+
+# Vim
 make_link vimrc         .vimrc
+
+# Zsh
 make_link zsh/zshrc     .zshrc
 make_link zsh/zlogin    .zlogin
 make_link zsh/zlogout   .zlogout
 make_link zsh/zpreztorc .zpreztorc
 make_link zsh/zprofile  .zprofile
 make_link zsh/zshenv    .zshenv
+
+# Aliases
 make_link public_aliases.sh .public_aliases.sh
+
+# Input options for terminal
 make_link inputrc       .inputrc
+
+# git
+make_link gitconfig       .gitconfig
+
+# VSCode
 make_link vscode/settings.json .config/VSCodium/User/settings.json
 make_link \
   vscode/snippets/javascript.json \
